4e21aea10fafea622660c404d2cffc16d95fe409
Thomas Steinbach <thomas.steinbach@aikq.de>
2016-05-24 21:44:57 +0200

    first working Dockercontext

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # docker_xwiki
-The Dockerfile context for the XWiki image.
+
+Dockerfile for XWiki
+
+## Usage
+
+See `docker-compose.yml` for example.
+
+## Variables
+
+### Tomcat configuration
+
+| Variable | Default | Description|
+|-----------|---|----|
+| TOMCAT_RAM | 1024m | equivalent to CATALINA_OPTS='-Xmx1024m' |
+
+### xwiki.cfg parameters
+
+All variable like `XWIKI_PARAM_NAME` are equivalents of the configuration parameters in xwiki.cfg like `xwiki.param.name`.
+
+| Variable | Default | Description|
+|-----------|---|----|
+| XWIKI_HOME | http://localhost:8080 | The domain name to use when creating URLs to the default wiki. |
+| XWIKI_AUTHENTICATION_COOKIEDOMAINS | localhost | For which part of the Wiki domain the cookie is valid for (E.g. if set to 'mydomain.com' it is also valid for 'xwiki1.mydomain.com', 'xwiki2.mydomain.com' and so on.) |
+| XWIKI_URL_PROTOCOL | http | Force the protocol to use in the generated URLs. |
+
+### xwiki.properties parameters
+
+All variable like `XWIKI_PARAM_NAME` are equivalents of the configuration parameters in xwiki.properties like `xwiki.param.name`.
+
+| Variable | Default | Description|
+|-----------|---|----|
+| MAIL_SENDER_HOST | localhost | SMTP host when sending emails. |
+| MAIL_SENDER_PORT | 25 | SMTP port when sending emails. |
+| MAIL_SENDER_FROM | john@doe.com | From email address to use. |
+| XWIKI_SENDER_USERNAME | someuser | Username to authenticate on the SMTP server. |
+| XWIKI_SENDER_PASSWORD | somepassword | Password to authenticate on the SMTP server. |
+
+## Backup
+
+* Backup permanentDirectory `/xwiki_data`
+* Backup Database
---
+++ files/system/start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function xwiki_first_start(){
+  echo "CATALINA_OPTS='-Xmx${TOMCAT_RAM:-1024m}'" >> /usr/local/tomcat/bin/setenv.sh
+
+  configure_xwiki
+
+  touch /usr/local/tomcat/.first_start_competed
+}
+
+function configure_xwiki(){
+  xwiki-set-cfg 'xwiki.store.attachment.hint' 'file'
+  xwiki-set-cfg 'xwiki.store.attachment.versioning.hint' 'file'
+  xwiki-set-cfg 'xwiki.store.attachment.recyclebin.hint' 'file'
+  xwiki-set-cfg 'xwiki.virtual.usepath' '0'
+  xwiki-set-cfg 'xwiki.home' "${XWIKI_HOME:-http://localhost:8082}"
+  xwiki-set-cfg 'xwiki.url.protocol' "${XWIKI_URL_PROTOCOL:-http}"
+  xwiki-set-cfg 'xwiki.webapppath' ''
+  xwiki-set-cfg 'xwiki.showviewaction' '0'
+  xwiki-set-cfg 'xwiki.authentication.validationKey' "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+  xwiki-set-cfg 'xwiki.authentication.encryptionKey' "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+  xwiki-set-cfg 'xwiki.authentication.cookiedomains' "${XWIKI_AUTHENTICATION_COOKIEDOMAINS:-localhost}"
+  xwiki-set-cfg 'xwiki.stats.default' '1'
+  xwiki-set-cfg 'xwiki.editcomment.suggested' '0'
+
+  xwiki-set-property 'environment.permanentDirectory' '/xwiki_data'
+  xwiki-set-property 'mail.sender.host' "${MAIL_SENDER_HOST:-localhost}"
+  xwiki-set-property 'mail.sender.port' "${MAIL_SENDER_PORT:-25}"
+  xwiki-set-property 'mail.sender.from' "${MAIL_SENDER_FROM:-john@doe.com}"
+  xwiki-set-property 'mail.sender.username' "${XWIKI_SENDER_USERNAME:-someuser}"
+  xwiki-set-property 'mail.sender.password' "${XWIKI_SENDER_PASSWORD:-somepassword}"
+}
+
+if [[ ! -f /usr/local/tomcat/.first_start_competed ]]; then
+  xwiki_first_start
+fi
+
+# wait for mysql
+sleep 15
+
+catalina.sh run
